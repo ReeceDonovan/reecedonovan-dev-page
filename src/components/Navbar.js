107228b285ae9a82166981efd8eeb8b0eb8ac3aa
Reece Donovan <reecedonovan99@gmail.com>
2020-10-03 23:59:18 +0100

    Added Nav, Nav captions and Nav styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,65 +4,90 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
-  //   const [click, setClick] = useState(false);
-  //   const handleClick = () => setClick(!click);
+  //   const [hovered, setHovered] = useState(false);
+  //   const toggleHover = () => setHovered(!hovered);
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="navbar-logo">
-          <i className="fas fa-rupee-sign">EECE</i>
+          <i className="fas fa-rupee-sign" />
         </Link>
-        <div className="navbar-container">
-          <ul className="nav-menu">
-            <li className="nav-item">
-              <Link to="/">
-                <i className="far fa-home-alt" />
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/about">
-                <i className="far fa-user" />
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/projects">
-                <i className="far fa-book-open" />
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact">
-                <i className="far fa-envelope" />
-              </Link>
-            </li>
-          </ul>
-          <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-              <i className="fas fa-rupee-sign">EECE</i>
+
+        <ul className="nav-menu internal">
+          <li className="nav-item">
+            <Link to="/">
+              <i
+                // onMouseEnter={toggleHover}
+                // onMouseLeave={toggleHover}
+                className="far fa-home-alt"
+              />
             </Link>
-            <ul className="nav-menu">
-              <li className="nav-item">
-                <Link to="/">
-                  <i className="far fa-home-alt" />
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/about">
-                  <i className="far fa-user" />
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/projects">
-                  <i className="far fa-book-open" />
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contact">
-                  <i className="far fa-envelope" />
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+            <div className="item-caption">Home</div>
+          </li>
+          <li className="nav-item">
+            <Link to="/about">
+              <i
+                // onMouseEnter={toggleHover}
+                // onMouseLeave={toggleHover}
+                className="far fa-user"
+              />
+            </Link>
+            <div className="item-caption">About</div>
+          </li>
+          <li className="nav-item">
+            <Link to="/projects">
+              <i
+                // onMouseEnter={toggleHover}
+                // onMouseLeave={toggleHover}
+                className="far fa-book-open"
+              />
+            </Link>
+            <div className="item-caption">Projects</div>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact">
+              <i
+                // onMouseEnter={toggleHover}
+                // onMouseLeave={toggleHover}
+                className="far fa-envelope"
+              />
+            </Link>
+            <div className="item-caption">Contact</div>
+          </li>
+        </ul>
+
+        <ul className="nav-menu external">
+          <li className="nav-item">
+            <Link to="/github">
+              <i
+                // onMouseEnter={toggleHover}
+                // onMouseLeave={toggleHover}
+                className="far fa-code-branch"
+              />
+            </Link>
+            <div className="item-caption">Github</div>
+          </li>
+          <li className="nav-item">
+            <Link to="/insta">
+              <i
+                // onMouseEnter={toggleHover}
+                // onMouseLeave={toggleHover}
+                className="fab fa-instagram"
+              />
+            </Link>
+            <div className="item-caption">Instagram</div>
+          </li>
+          <li className="nav-item">
+            <Link to="/email">
+              <i
+                // onMouseEnter={toggleHover}
+                // onMouseLeave={toggleHover}
+                className="far fa-at"
+              />
+            </Link>
+            <div className="item-caption">Email</div>
+          </li>
+        </ul>
       </nav>
     </>
   );
